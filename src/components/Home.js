27f553c80f0c9207ec73957fd7d44fe7191f6b50
TomasJefferson <crazyhealer1205@gmail.com>
2022-01-13 17:39:54 +0800

    make some parts of home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,11 @@
-import React, { useEffect } from "react";
+import React from "react";
 import "../assets/css/home.scss";
 import { Container, Row, Col, Button } from "react-bootstrap";
 import TopContributePresail from "../assets/images/top-contribute-presail.png";
-import Shape2 from "../assets/images/shape-2.png";
 import styled from "styled-components";
+import { FaShieldAlt, FaEuroSign, FaWifi, FaUserSecret, FaBezierCurve, FaLayerGroup, FaCrown, FaGalacticRepublic, FaKey, FaHandshake } from "react-icons/fa";
+
+import Shape2 from "../assets/images/shape-2.png";
 import Xcap_Logo from "../assets/images/clients/0xcap-logo.png";
 import Vc_Logo from "../assets/images/clients/316vc-logo.png";
 import Alpha_Logo from "../assets/images/clients/alpha-logo.png";
@@ -24,6 +26,7 @@ import Skyx_Logo from "../assets/images/clients/skyx-logo.png";
 import Trust_Logo from "../assets/images/clients/trust-logo.png";
 import Vespertine_Logo from "../assets/images/clients/vespertine-logo.png";
 import Ysc_Logo from "../assets/images/clients/ysc-logo.png";
+import MockupPresail from "../assets/images/mockuppresail.png";
 
 const ImageContainer = styled.div`
   display: flex;
@@ -37,10 +40,14 @@ const ImageWrapper = styled.div`
   flex-grow: 1;
 `;
 
+const SecurityContent = styled.div`
+  padding-top: 4%;
+  display: flex;
+  align-items: center;
+`;
+
 export const Home = () => {
-  useEffect(() => {
-    console.log("page start");
-  });
+
   return (
     <div style={{ position: "relative" }}>
       <img src={Shape2} alt="Shape2" className="shape2-img"></img>
@@ -156,6 +163,160 @@ export const Home = () => {
             </ImageContainer>
           </Col>
           <Col xs={0} sm={0} md={1} xl={1} lg={1}></Col>
+        </Row>
+      </Container>
+      <Container className="security-content">
+        <Row>
+          <Col xs={0} sm={0} md={2} lg={2} xl={2}></Col>
+          <Col xs={12} sm={12} md={8} lg={8} xl={8}>
+            <p className="about-title security-title" style={{ color: "#fff" }}>Security is <span style={{ color: "rgba(41, 169, 235, 1)" }}>paramount</span></p>
+            <p className="about-title ease-of-use">
+              We are proven safe to use, and compliant as well.
+            </p>
+          </Col>
+          <Col xs={0} sm={0} md={2} lg={2} xl={2}></Col>
+        </Row>
+        <Row style={{ marginTop: "5%" }}>
+          <Col xs={0} sm={0} md={3} lg={3} xl={3}></Col>
+          <Col xs={12} sm={12} md={3} lg={3} xl={3}>
+            <SecurityContent>
+              <FaShieldAlt className="icons-style" size={30} />
+              <p className="about-title icon-description">All data is AES-GCM encrypted. Simply put, it’s secure.</p>
+            </SecurityContent>
+          </Col>
+          <Col xs={12} sm={12} md={3} lg={3} xl={3}>
+            <SecurityContent>
+              <FaEuroSign className="icons-style" size={30} />
+              <p className="about-title icon-description">Enterprise level Secure Access Service Edge powered by Cloudflare</p>
+            </SecurityContent>
+          </Col>
+          <Col xs={0} sm={0} md={3} lg={3} xl={3}></Col>
+        </Row>
+        <Row style={{ marginTop: "5%" }}>
+          <Col xs={0} sm={0} md={3} lg={3} xl={3}></Col>
+          <Col xs={12} sm={12} md={3} lg={3} xl={3}>
+            <SecurityContent>
+              <FaWifi className="icons-style" size={40} />
+              <p className="about-title icon-description">Our team has C-level experience handling security at international companies.</p>
+            </SecurityContent>
+          </Col>
+          <Col xs={12} sm={12} md={3} lg={3} xl={3}>
+            <SecurityContent>
+              <FaUserSecret className="icons-style" size={30} />
+              <p className="about-title icon-description">Enjoy zero tracking pixels, no Google, no IP tracking.</p>
+            </SecurityContent>
+          </Col>
+          <Col xs={0} sm={0} md={3} lg={3} xl={3}></Col>
+        </Row>
+      </Container>
+      <Container className="about-container">
+        <Row className="managing">
+          <Col className="managing-title-content" xs={12} sm={12} md={7} lg={7} xl={7}>
+            <Row>
+              <Col>
+                <p className="managing-text">Managing deals</p>
+                <p className="about-title how-pros-text">Get Your <span className="about-title manage-platform-text">overview</span>
+                </p>
+                <p className="about-title ease-of-use">
+                  Presail helps VC firms spend 90% less time on organizing presale deals and more time on finding deals.
+                </p>
+              </Col>
+            </Row>
+            <Row style={{ paddingTop: "3%"}}>
+              <Col xs={12} sm={12} md={6} lg={6} xl={6}>
+                <SecurityContent>
+                  <FaBezierCurve className="icons-style" size={30} />
+                  <p className="about-title icon-description">Create, manage and distribute your deals to your investors.</p>
+                </SecurityContent>
+              </Col>
+              <Col xs={12} sm={12} md={6} lg={6} xl={6}>
+                <SecurityContent>
+                  <FaLayerGroup className="icons-style" size={30} />
+                  <p className="about-title icon-description">Your community will love you for choosing us.</p>
+                </SecurityContent>
+              </Col>
+            </Row>
+            <Row style={{ paddingTop: "3%"}}>
+              <Col xs={12} sm={12} md={6} lg={6} xl={6}>
+                <SecurityContent>
+                  <FaCrown className="icons-style" size={30} />
+                  <p className="about-title icon-description">Your community will love you for choosing us.</p>
+                </SecurityContent>
+              </Col>
+              <Col xs={12} sm={12} md={6} lg={6} xl={6}>
+                <SecurityContent>
+                  <FaGalacticRepublic className="icons-style" size={30} />
+                  <p className="about-title icon-description">Your community will love you for choosing us.</p>
+                </SecurityContent>
+              </Col>
+            </Row>
+          </Col>
+          <Col className="image-content" xs={12} sm={12} md={5} lg={5} xl={5}>
+            <img src={MockupPresail} alt="topImage" className="top-image" />
+          </Col>
+        </Row>
+      </Container>
+      <Container className="security-content">
+        <Row>
+          <Col xs={0} sm={0} md={2} lg={2} xl={2}></Col>
+          <Col xs={12} sm={12} md={8} lg={8} xl={8}>
+            <p className="managing-text">Managing deals</p>
+            <p className="about-title security-title" style={{ color: "#fff" }}>
+              Presail <span style={{ color: "rgba(41, 169, 235, 1)" }}>benefits</span>
+            </p>
+            <p className="about-title ease-of-use">
+              Presail helps VC firms spend 90% less time on organizing presale deals and more time on finding deals.
+            </p>
+          </Col>
+          <Col xs={0} sm={0} md={2} lg={2} xl={2}></Col>
+        </Row>
+        <Row style={{ marginTop: "5%" }}>
+          <Col xs={0} sm={0} md={3} lg={3} xl={3}></Col>
+          <Col xs={12} sm={12} md={3} lg={3} xl={3}>
+            <SecurityContent>
+              <FaBezierCurve className="icons-style" size={40} />
+              <p className="about-title icon-description">Create, manage and distribute your deals to your investors.</p>
+            </SecurityContent>
+          </Col>
+          <Col xs={12} sm={12} md={3} lg={3} xl={3}>
+            <SecurityContent>
+              <FaLayerGroup className="icons-style" size={30} />
+              <p className="about-title icon-description">Your community will love you for choosing us.</p>
+            </SecurityContent>
+          </Col>
+          <Col xs={0} sm={0} md={3} lg={3} xl={3}></Col>
+        </Row>
+        <Row style={{ marginTop: "5%" }}>
+          <Col xs={0} sm={0} md={3} lg={3} xl={3}></Col>
+          <Col xs={12} sm={12} md={3} lg={3} xl={3}>
+            <SecurityContent>
+              <FaCrown className="icons-style" size={30} />
+              <p className="about-title icon-description">Stay on brand. Use your own domain.</p>
+            </SecurityContent>
+          </Col>
+          <Col xs={12} sm={12} md={3} lg={3} xl={3}>
+            <SecurityContent>
+              <FaGalacticRepublic className="icons-style" size={40} />
+              <p className="about-title icon-description">Simplified distribution with disperse or other bulksenders.</p>
+            </SecurityContent>
+          </Col>
+          <Col xs={0} sm={0} md={3} lg={3} xl={3}></Col>
+        </Row>
+        <Row style={{ marginTop: "5%" }}>
+          <Col xs={0} sm={0} md={3} lg={3} xl={3}></Col>
+          <Col xs={12} sm={12} md={3} lg={3} xl={3}>
+            <SecurityContent>
+              <FaKey className="icons-style" size={30} />
+              <p className="about-title icon-description">We are safe to use. The process is secure.</p>
+            </SecurityContent>
+          </Col>
+          <Col xs={12} sm={12} md={3} lg={3} xl={3}>
+            <SecurityContent>
+              <FaHandshake className="icons-style" size={40} />
+              <p className="about-title icon-description">It's a partnership, you get continuous development.</p>
+            </SecurityContent>
+          </Col>
+          <Col xs={0} sm={0} md={3} lg={3} xl={3}></Col>
         </Row>
       </Container>
     </div>
